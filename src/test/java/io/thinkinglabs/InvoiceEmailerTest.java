6e76c5fc7026e41c9bed177af0fbd090edc5e248
Thierry de Pauw <thierry@thinkinglabs.io>
2018-11-06 10:47:22 +0100

    reformat InvoiceEmailerTest + rename classUnderTest to sut

--- src/test/java/io/thinkinglabs/InvoiceEmailerTest.java
+++ src/test/java/io/thinkinglabs/InvoiceEmailerTest.java
@@ -11,18 +11,15 @@ public class InvoiceEmailerTest
 {
     @Test
     public void confirmationMail() {
-        InvoiceEmailer classUnderTest = new FeatureAwareFactory(
-                new FeatureDecisions(
-                        new Features(Collections.emptyMap()))).invoiceEmailer();
-        assertEquals(new Email("12345: bla bla bla"), classUnderTest.generateInvoiceEmail(new Invoice("12345")));
+        InvoiceEmailer sut = new FeatureAwareFactory(new FeatureDecisions(new Features(Collections.emptyMap())))
+                .invoiceEmailer();
+        assertEquals(new Email("12345: bla bla bla"), sut.generateInvoiceEmail(new Invoice("12345")));
     }
 
     @Test
     public void confirmationMailWithOrderCancellation() {
-        InvoiceEmailer classUnderTest = new FeatureAwareFactory(
-                new FeatureDecisions(
-                    new Features(
-                        Map.of("next-gen-ecomm", Boolean.TRUE)))).invoiceEmailer();
-        assertEquals(new Email("12345: bla bla bla\nCancel order"), classUnderTest.generateInvoiceEmail(new Invoice("12345")));
+        InvoiceEmailer sut = new FeatureAwareFactory(new FeatureDecisions(new Features(Map.of("next-gen-ecomm", Boolean.TRUE))))
+                .invoiceEmailer();
+        assertEquals(new Email("12345: bla bla bla\nCancel order"), sut.generateInvoiceEmail(new Invoice("12345")));
     }
 }
